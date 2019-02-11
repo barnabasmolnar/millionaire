f93960f08099bf11ef5c0888f46df248591669d9
Barnabás Molnár <barna.molnar90@gmail.com>
2019-02-11 01:27:50 +0100

    Axios and redux-thunk added as dependencies.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,16 @@
+import axios from "axios";
+
 // Actions constants
 export const SET_CATEGORIES = "SET_CATEGORIES";
 
 // Action creators
-export const setCategories = categories => ({ type: SET_CATEGORIES, categories });
+export const setCategories = categories => ({
+    type: SET_CATEGORIES,
+    categories
+});
+
+// Async
+export const fetchCategories = () => dispatch =>
+    axios
+        .get("https://opentdb.com/api_category.php")
+        .then(({ data }) => dispatch(setCategories(data.trivia_categories)));
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { compose, applyMiddleware, createStore } from "redux";
 import App from "./App";
 import reducers from "./reducers";
+import thunk from "redux-thunk";
 import * as serviceWorker from "./serviceWorker";
+import { fetchCategories } from "./actions";
 
-const store = createStore(
-    reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+// store.dispatch(fetchCategories());
 
 ReactDOM.render(
     <Provider store={store}>
