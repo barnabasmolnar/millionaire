2cf89b27612c5a0d90d43503c52a93847fd9038c
Barnabás Molnár <barna.molnar90@gmail.com>
2019-03-03 02:14:33 +0100

    Boilerplate largely set up for UI development.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class App extends Component {
     render() {
-        return <div className="App">Hello there</div>;
+        return (
+            <div className="container mx-auto">
+                <h1>Tailwind installed!</h1>
+            </div>
+        );
     }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
 import { Provider } from "react-redux";
 import { compose, applyMiddleware, createStore } from "redux";
 import App from "./App";
